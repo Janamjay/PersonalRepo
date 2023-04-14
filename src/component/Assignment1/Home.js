0bb573465fd68e41d567b3ab157a770275a7b30b
Janammjay <janammjayk6@gmail.com>
2023-04-14 20:14:20 +0530

    assignment completed

---
+++ src/component/Assignment1/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div>
+        <ul>
+            <li><Link to ="/">Home</Link></li>
+            <li><Link to ="/contact">Contact</Link></li>          
+        </ul>
+      <h1>This is Home page</h1>
+    </div>
+  )
+}
